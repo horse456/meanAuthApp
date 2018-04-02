5dd1da8c126dd8876702c4c7f67ddd94bcbaf0fc
horse456 <cn45604@aliyun.com>
2018-04-02 21:55:30 +0800

    step9 protected requests and auth guard

--- angular.scr/src/app/services/auth.service.ts
+++ angular.scr/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -23,6 +24,15 @@ export class AuthService {
       .map( res => res.json());
   }
 
+  getProfile () {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/users/profile', {headers: headers})
+      .map( res => res.json());
+  }
+
   storeUserData (token, user) {
     localStorage.setItem('id_token', token);
     localStorage.setItem('user', JSON.stringify(user));
@@ -30,6 +40,15 @@ export class AuthService {
     this.user = user
   }
 
+  loadToken () {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  loggedIn () {
+    return tokenNotExpired('id_token');
+  }
+  
   logout() {
     this.authToken = null;
     this.user = null;
