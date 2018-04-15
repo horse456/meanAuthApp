cf8aef05e8613bb98a59ebcbceba0a62bb7fcda5
horse456 <cn45604@aliyun.com>
2018-04-15 15:24:08 +0800

    use tslint to clear error

--- angular.scr/src/app/components/dashboard/operation/operation.component.ts
+++ angular.scr/src/app/components/dashboard/operation/operation.component.ts
@@ -12,7 +12,7 @@ export class OperationComponent implements OnInit {
 
   submited: boolean;
   edit: boolean;
-  operationMessage:any;
+  operationMessage: any;
   operationId: string;
   operation: object;
   todo: string[];
@@ -20,7 +20,7 @@ export class OperationComponent implements OnInit {
   desc: string;
   result: boolean;
 
-  @Output() onOperationId = new EventEmitter<string>();
+  @Output() OperationId = new EventEmitter<string>();
   @Input() getOperationId: string;
 
   constructor(
@@ -34,74 +34,74 @@ export class OperationComponent implements OnInit {
   }
 
   onTodos (todos: string[]) {
-    this.todo = todos
+    this.todo = todos;
   }
 
   onDones (dones: boolean[]) {
-    this.done = dones
+    this.done = dones;
   }
 
   onOperationFormSubmit() {
     // done[] have one false, then the result is false
     this.result = true;
     for ( let item of this.done) {
-      if(!item) { this.result = false};  
-    };
+      if (!item) { this.result = false; }
+    }
     const doc = this.operation = {
                               step: this.todo,
                               done: this.done,
                               result: this.result};
-    console.log('operation Form Submit: ',this.operation);
+    console.log('operation Form Submit: ', this.operation);
 
     this.dashboardService.submitOperation(doc).subscribe( data => {
       if (data.success) {
-        this.flashMessage.show("Operation was now submited  ", {cssClass: 'alert-success', timeout: 3000});
-       
+        this.flashMessage.show('Operation was now submited  ', {cssClass: 'alert-success', timeout: 3000});
+
         // make flagged to hidden/show Form
         this.submited = true;
-        
+
         // output the rehearsalMessage
         // const messages = JSON.stringify(doc);
         // console.log(messages);
         // this.operationMessage= messages.slice(2,messages.length-1).split("],");
         // this.operationMessage[0] += ']';
         // console.log('submit operationMessage: ',this.operationMessage);
-        
+
         // get the smartId to edit
         this.operationId = data.operation._id;
-        console.log('operationId: '+this.operationId)
-      
-        // pass the rehearsal Id to dashboard
-        this.onOperationId.emit(this.operationId);
-        
-        // this.router.navigate(['/dashboard']);
-        
-      } else {
-        this.flashMessage.show("add operation went wrong ", {cssClass: 'alert-danger', timeout: 3000});
-      }
-    })
+        console.log('operationId: ' + this.operationId);
 
-    
+        // pass the rehearsal Id to dashboard
+        this.OperationId.emit(this.operationId);
+
+        // this.router.navigate(['/dashboard']);
+
+      } else {
+        this.flashMessage.show('add operation went wrong ', {cssClass: 'alert-danger', timeout: 3000});
+      }
+    });
+
+
   }
 
-  onOperationFormUpdate(){
+  onOperationFormUpdate() {
     // done[] have one false, then the result is false
-    if (this.done.indexOf(false) < 0 ) {this.result = true} else { this.result = false};
+    if (this.done.indexOf(false) < 0 ) {this.result = true; } else { this.result = false; }
 
     const doc = this.operation =  {
                                   step: this.todo,
                                   done: this.done,
                                   result: this.result};
     const Id = this.operationId = this.getOperationId;
-    console.log('update operationId:  ',Id);
-    this.dashboardService.updateOperation(doc,Id).subscribe(data => {
+    console.log('update operationId:  ', Id);
+    this.dashboardService.updateOperation(doc, Id).subscribe(data => {
       if (data.success) {
-        this.flashMessage.show("Operation are now updated  ", {cssClass: 'alert-success', timeout: 3000});
-      
+        this.flashMessage.show('Operation are now updated  ', {cssClass: 'alert-success', timeout: 3000});
+
         // make flagged to hidden/show operation Form
         this.submited = true;
         this.edit = false;
-        
+
         // output the operatioMessage
         // const messages = JSON.stringify(doc);
         // this.operationMessage= messages.slice(2,messages.length-1).split("],");
@@ -109,15 +109,15 @@ export class OperationComponent implements OnInit {
         // console.log('update operatinMessage: '+ this.operationMessage)
 
         // pass the rehearsal Id to parent module
-        this.onOperationId.emit(this.operationId);
-        
+        this.OperationId.emit(this.operationId);
+
       } else {
-        this.flashMessage.show("update operation went wrong ", {cssClass: 'alert-danger', timeout: 3000});
+        this.flashMessage.show('update operation went wrong ', {cssClass: 'alert-danger', timeout: 3000});
       }
-    })
+    });
 
   }
-  
+
   onOperationFormEdit() {
     // show the Form
     this.submited = false;
