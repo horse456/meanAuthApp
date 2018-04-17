37f9928ed74d7e47f6a44600ed39940b3ba1b4fc
horse456 <cn45604@aliyun.com>
2018-04-17 16:09:32 +0800

    fixed the errors

--- angular.scr/src/app/components/dashboard/dynamic/dynamic.component.ts
+++ angular.scr/src/app/components/dashboard/dynamic/dynamic.component.ts
@@ -47,19 +47,19 @@ export class DynamicComponent implements OnInit {
 
   // add new dynamic form
   addDynamic() {
-    if (this.dynamic.length === 1) {
-      this.dynamic.pop();
-      this.dynamic.push('a');
-    }
     this.dynamics = [this.state, this.action, this.reward, this.done];
     if (this.state && this.action && this.reward && this.done) {
-          this.dynamicTitle = 'a' + this.dynamic.length;
-          this.dynamic.push(this.dynamicTitle);
-          console.log('show number: ', this.dynamic);
-          console.log('dynamic pass data: ', this.dynamics);
-          this.Dynamics.emit(this.dynamics);
-          // after clicked, make flag \
-          this.addClick = true;
+      // create one unquie number, make the dynamicTitle is unquired
+      const d = new Date();
+      this.dynamicTitle = this.dynamic[this.dynamic.length - 1];
+
+      // create new form
+      this.dynamic.push('a' + d.getTime());
+      console.log('show dynamic items: ', this.dynamic);
+      console.log('dynamic pass data: ', this.dynamics);
+      this.Dynamics.emit(this.dynamics);
+      // after clicked, make flag, and hidden the add button
+      this.addClick = true;
     } else {
       this.flashMessage.show('you cant add dynamic form ', {cssClass: 'alert-danger', timeout: 3000});
     }
@@ -71,7 +71,7 @@ export class DynamicComponent implements OnInit {
       // delete the selected dynamic form by dynamicTitle
       const i = this.dynamic.indexOf(this.dynamicTitle);
       if (this.addClick) {
-        this.dynamic.splice(i - 1, 1);
+        this.dynamic.splice(i, 1);
       } else {
         this.dynamic.pop();
       }
@@ -84,6 +84,8 @@ export class DynamicComponent implements OnInit {
       this.delDynamics.emit(this.dynamics);
       this.submited = false;
       this.finished = false;
+      // show the add button
+      this.addClick = false;
     }
     console.log(this.dynamic);
 
